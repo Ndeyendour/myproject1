1abfaa9a26c153c3827d52bced4cea5268ab3734
Ndeyendour <ndeyefatou023@gmail.com>
2024-07-23 03:04:08 +0200

    bonsoir

---
+++ asana/lib/auth/auth_page.dart
@@ -0,0 +1,28 @@
+import 'package:asana/screen/SingUP.dart';
+import 'package:asana/screen/login.dart';
+import 'package:flutter/material.dart';
+
+class Auth_Page extends StatefulWidget {
+  Auth_Page({super.key});
+
+  @override
+  State<Auth_Page> createState() => _Auth_PageState();
+}
+
+class _Auth_PageState extends State<Auth_Page> {
+  bool a = true;
+  void to() {
+    setState(() {
+      a = !a;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (a) {
+      return LogIN_Screen(to);
+    } else {
+      return SignUp_Screen(to);
+    }
+  }
+}
